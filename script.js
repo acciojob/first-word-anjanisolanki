91df1e2c591cf62be93878584f796d3ad1560235
anjanisolanki <anjanisolanki1220@gmail.com>
2026-03-05 19:43:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
-  // your code here
+  s = s.trim();
+	let result = "";
+	for(let i = 0; i < s.length; i++){
+		if(s[i] === " "){
+			break;
+		}
+		result = result + s[i];
+	}
+	return result;
 }
 
 // Do not change the code below
